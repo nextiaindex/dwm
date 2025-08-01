770691d3e97aff4e7cde51f1bec76800da2d4a51
NextiaIndex <nextiaindex@protonmail.com>
2025-08-01 19:48:18 +0700

    change the borderpx to 1 and inactive border color

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int gappx     = 12;        /* gaps between windows */
 static const int showbar            = 1;        /* 0 means no bar */
@@ -18,7 +18,7 @@ static const char col_gray5[]       = "#333c50";
 static const char col_cyan[]        = "#f28779";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray1 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 	[SchemeStatus]  = { col_gray3, col_gray1,  "#000000"  }, // Statusbar right {text,background,not used but cannot be empty}
 	[SchemeTagsSel]  = { col_gray3, col_gray5,  "#000000"  }, // Tagbar left selected {text,background,not used but cannot be empty}
