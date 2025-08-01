9142d6026668e475ffb7a357a1e853226eca822a
NextiaIndex <nextiaindex@protonmail.com>
2025-08-01 08:19:20 +0700

    assign MODKEY + P to rofidruncmd

--- config.h
+++ config.h
@@ -60,7 +60,7 @@ static const char *termcmd[]  = { "st", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_p,      spawn,          {.v = rofidruncmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
