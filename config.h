d3e27832f373a7af2a99ce4f37d721ac81399d17
NextiaIndex <nextiaindex@protonmail.com>
2025-08-01 17:59:24 +0700

    move the comments

--- config.h
+++ config.h
@@ -6,8 +6,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "UnifontExMono:size=12" };
-// im not using dmenu. This line exists to prevent compiler errors.
-static const char dmenufont[]		= { "monospace:size=10" };
+static const char dmenufont[]		= { "monospace:size=10" }; // im not using dmenu. This line exists to prevent compiler errors.
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -57,8 +56,7 @@ static const Layout layouts[] = {
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
 
 /* commands */
-static char dmenumon[2] = "0";
-// im not using dmenu. This line exist to prevent compiler errors
+static char dmenumon[2] = "0"; // im not using dmenu. This line exist to prevent compiler errors
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-p", "-fn", dmenufont, NULL };
 static const char *rofidruncmd[]  = { "rofi", "-show", "drun", NULL };
 static const char *termcmd[]  = { "st", NULL };
