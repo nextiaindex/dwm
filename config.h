95fa697cd5831699868cb1a39f8cc251b15532a6
NextiaIndex <nextiaindex@protonmail.com>
2025-08-01 08:40:58 +0700

    add comma after XK_Print

--- config.h
+++ config.h
@@ -95,9 +95,9 @@ static const Key keys[] = {
 	TAGKEYS(                        XK_8,										7)
 	TAGKEYS(                        XK_9,										8)
 	{ MODKEY|ShiftMask,             XK_q,      					quit,			{0} },
-	{ 0, 							XK_Print   					spawn,			SHCMD("~/.local/bin/maim-plain") },
-	{ MODKEY, 						XK_Print   					spawn,			SHCMD("~/.local/bin/maim-fancy") },
-	{ ControlMask, 					XK_Print   					spawn,			SHCMD("~/.local/bin/maim-full") },
+	{ 0, 							XK_Print,   				spawn,			SHCMD("~/.local/bin/maim-plain") },
+	{ MODKEY, 						XK_Print,   				spawn,			SHCMD("~/.local/bin/maim-fancy") },
+	{ ControlMask, 					XK_Print,   				spawn,			SHCMD("~/.local/bin/maim-full") },
 	{ 0,							XF86XK_MonBrightnessUp,		spawn,			SHCMD("brightnessctl set +5%") },
 	{ 0,							XF86XK_MonBrightnessDown,	spawn,			SHCMD("brightnessctl set -5%") },
 	{ 0,							XF86XK_AudioRaiseVolume, 	spawn,			SHCMD("pactl set-sink-volume 0 +1%") },
